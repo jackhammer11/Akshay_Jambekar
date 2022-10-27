a480c3273914aa4bd0d8dcc4665a33dac23891bf
Akshay Jambekar <51314668+jackhammer11@users.noreply.github.com>
2022-10-27 18:24:04 +0530

    Add files via upload

---
+++ Assignment1/Assignment1_1.java
@@ -0,0 +1,103 @@
+public class Assignment1_1 {
+
+    public static void main(String[] args) {
+
+        int n = 11;
+         /*I */
+        for(int i = 0;i<n;i++)
+        { 
+        for (int j =0;j<n;j++)
+        {
+            if(i==0 && j>0 && j<(n-1) || i==(n-1) && j>0  && j<(n-1)|| j==(n-1)/2)
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.print("\t ");
+         /*N */
+        for (int j =0;j<n;j++)
+        {
+            if(j==0|| j==(n-1) || i==j || j==(n-1)/2 && i==j)
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+
+        System.out.print("\t ");
+      
+        /*E */
+
+        for (int j =0;j<n;j++)
+        {
+            if(i==0 || j==0 ||i==(n-1)/2 || i==(n-1))
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.print("\t ");
+        /*U */
+        for (int j =0;j<n;j++)
+        {
+            if(j==0 && i<(n-1) || j==3*n/4  && i<(n-1)|| i==(n-1)&& j>0 && j<3*n/4)
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+
+        System.out.print("\t");
+        /*R */
+        for (int j =0;j<n;j++)
+        {
+            if( j==0  ||i==0 &&j<(n-1)/2 || i==(n)/2 &&j<(n)/2 && j>0 || j==n/2 && i>0 && i<(n)/2||i==j && j>n/2 && j<=(n-1))
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+
+        System.out.print("\t");
+        /* O */        
+        for (int j =0;j<n;j++)
+        {
+            if(i==0 && j>0 && j<(n-1) || j==0 && i>0 && i<(n-1) || j==(n-1) && i>0 && i<(n-1) ||i==(n-1) && j>0 && j<(n-1) ) 
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.print("\t");
+        /* N */
+        for (int j =0;j<n;j++)
+        {
+            if(j==0|| j==(n-1) || i==j || j==(n-1)/2 && i==j)
+            {
+        System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println(" ");
+    }
+   
+
+    
+}
+}
---
+++ Assignment1/Assignment1_2.java
@@ -0,0 +1,22 @@
+public class Assignment1_2 {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+       for(int i = 0;i<n;i++)
+       { 
+        for (int j=0;j<n;j++)
+        {
+            if(j<n)
+            {
+                System.out.print(i+1);
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+
+       }
+    }
+    
+}
